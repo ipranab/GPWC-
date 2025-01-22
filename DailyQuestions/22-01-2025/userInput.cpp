729d45c8ed1ee1c2de3756574f9641daf5ff4fc7
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2025-01-22 11:07:02 +0530

    Create userInput.cpp

---
+++ DailyQuestions/22-01-2025/userInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1;
+	int num2;
+	int sum=0;
+	cout<<"enter first number"<<endl;
+	cin>>num1; //similar to scanf in c
+	/* it is used to take input from the user*/
+	cout<<"enter the second number"<<endl;
+	cin>>num2;
+	
+	sum=num1+num2;
+	cout<<"sum:"<<sum<<endl;
+	return 0;
+	
+}
