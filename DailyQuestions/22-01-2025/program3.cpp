25cb82dc511763551a6ebd5a9c8ae31bc4419b86
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2025-01-22 11:07:31 +0530

    Create program3.cpp

---
+++ DailyQuestions/22-01-2025/program3.cpp
@@ -0,0 +1,19 @@
+//write a program to print size of- char,int,float,double,short int, long int
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	int a;
+	float b;
+	double c;
+	short int aa;
+	long int aaa;
+	cout << "Size of char:"<< sizeof(ch)<< endl;
+	cout << "Size of integer:"<< sizeof(a)<< endl;
+	cout << "Size of float:"<< sizeof(b)<< endl;
+	cout << "Size of double:"<< sizeof(c)<< endl;
+	cout << "Size of short int:"<< sizeof(aa)<< endl;
+	cout << "Size of long int:"<< sizeof(aaa)<< endl;
+	return 0;
+	
+}
