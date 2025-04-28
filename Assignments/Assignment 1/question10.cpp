54d86183bbf8c19fd6e01203de8ffae21b234075
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2025-04-28 23:36:02 +0530

    Create question10.cpp

---
+++ Assignments/Assignment 1/question10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Test {
+public:
+    Test() { cout << "Constructor called"; }
+};
+
+int main() {
+    Test *t = new Test();
+    return 0;
+}
+
+//output- constructor called
