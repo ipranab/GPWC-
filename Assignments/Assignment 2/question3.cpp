202a47d081f409a19a27d3fc47be990eb907af81
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2025-04-28 23:50:28 +0530

    Create question3.cpp

---
+++ Assignments/Assignment 2/question3.cpp
@@ -0,0 +1,5 @@
+Let say, void setValues(int, int); and void getValues( ); are the public member functions for the class calculateArea{ ... };. Write the C++ statements to call both the
+functions using object(s) of that class.
+
+  Ans- obj1.setValues(5, 10);  // Setting values
+       obj1.getValues();       // Getting/printing values
