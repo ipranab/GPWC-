411e8ccabb68f4ff8499aa96ecb5f7be989c62ae
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2025-04-28 21:40:16 +0530

    Create question9.cpp

---
+++ Assignments/Assignment-1/question9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+class Test {
+public:
+    Test() { cout << "Constructor called"; }
+};
+
+int main() {
+    Test *t = (Test *)malloc(sizeof(Test));
+    return 0;
+}
+
+//output:- (No Output)
