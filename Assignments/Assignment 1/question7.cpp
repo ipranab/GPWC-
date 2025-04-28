6362f17b823552c7a3e400e76e712d035b733ce0
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2025-04-28 23:30:27 +0530

    Create question7.cpp

---
+++ Assignments/Assignment 1/question7.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class A {
+public:
+    A() { cout << "A "; }
+    ~A() { cout << "~A "; }
+};
+
+void func() {
+    static A obj;
+}
+
+int main() {
+    func();
+    func();
+    cout << "Main ";
+    return 0;
+}
+
+//output- A Main ~A
